cd5571445ae189ea2c7cee9826e0643d01db2fc4
Arii <muhammadari280702@gmail.com>
2023-04-03 21:24:08 +0700

    home

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -4,25 +4,28 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Fixedgear | {{$title}}</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-aFq/bzH65dt+w6FI2ooMVUpc+21e0SRygnTpmBvdBgSdnuTN7QbdgL+OapgHtvPp" crossorigin="anonymous">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
   </head>
-  <body>
-  <nav class="navbar navbar-expand-lg bg-body-tertiary">
-  <div class="container">
-    <a class="navbar-brand" href="/">Fixedgear</a>
+  <body> 
+    <nav class="navbar navbar-expand-lg bg-body-tertiary">
+  <div class="container-fluid">
+    <a class="navbar-brand px-5 fs-3 fw-bolder" href="#"><span class="text-warning">FIXED</span>GEAR</a>
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
     <div class="collapse navbar-collapse" id="navbarNav">
-      <ul class="navbar-nav">
-        <li class="nav-item">
-          <a class="nav-link {{ ($title === "Home") ? 'active' : '' }}" aria-current="page" href="/">Home</a>
+      <ul class="navbar-nav ms-auto fs-5">
+        <li class="nav-item px-3">
+          <a class="nav-link active" aria-current="page" href="#">Home</a>
         </li>
-        <li class="nav-item">
-          <a class="nav-link {{ ($title === "About") ? 'active' : '' }}" href="/about">About</a>
+        <li class="nav-item px-3">
+          <a class="nav-link" href="#">Features</a>
         </li>
-        <li class="nav-item">
-          <a class="nav-link {{ ($title === "Kontak") ? 'active' : '' }}" href="/contact">Kontak</a>
+        <li class="nav-item px-3">
+          <a class="nav-link" href="#">Pricing</a>
+        </li>
+        <li class="nav-item px-3">
+          <a class="nav-link disabled">Disabled</a>
         </li>
       </ul>
     </div>
