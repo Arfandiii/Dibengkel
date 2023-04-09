08e29ab7f7d0d67624734bc3381a436d90f43121
rndyrnlldy <201220093@gmail.com>
2023-04-10 00:11:06 +0700

    rendi

--- resources/views/service.blade.php
+++ resources/views/service.blade.php
@@ -2,4 +2,5 @@
 
 @section('container')
     <h1>Halaman Service</h1>
+    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Esse, atque exercitationem. Eum hic velit numquam quibusdam ea autem a rem dolores quas, quia quidem? Quibusdam ipsam aperiam temporibus molestias doloribus!</p>
 @endsection
